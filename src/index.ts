b635af2accbf38997d88f3be530397dd9820c6f6
mitsu3s <mitsu3s.dev@gmail.com>
2025-08-02 23:23:39 +0900

    Add metrics & custom span

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
-import "./otel-exporter";
 import express from "express";
-import { trace } from "@opentelemetry/api";
+import {
+  diag,
+  DiagConsoleLogger,
+  DiagLogLevel,
+  trace,
+} from "@opentelemetry/api";
+
+import "./otel-exporter";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get("/hello", (_req, res) => {
-  const tracer = trace.getTracer("manual");
-  const span = tracer.startSpan("manual-span-hello");
-  setTimeout(() => {
-    span.end();
-    res.send("Hello from GKE with manual span");
-  }, 100);
+app.get("/hello", async (_req, res) => {
+  const span = trace
+    .getTracer("ts-observability-app")
+    .startSpan("cpu-bound-operation");
+
+  // ↓ 簡単なCPU負荷処理（カスタムメトリクスが出しやすい）
+  const start = Date.now();
+  let total = 0;
+  for (let i = 0; i < 1e6; i++) total += Math.sqrt(i);
+  const duration = Date.now() - start;
+
+  span.end();
+
+  res.send(`Hello! CPU load done in ${duration}ms`);
 });
 
 app.listen(port, () => {
