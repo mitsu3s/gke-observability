a2039a474bb14bddcc4621a5982965966808acf1
mitsu3s <mitsu3s.dev@gmail.com>
2025-08-02 22:53:10 +0900

    Add metrics reader

--- src/otel-exporter.ts
+++ src/otel-exporter.ts
@@ -4,6 +4,15 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+
+const metricExporter = new OTLPMetricExporter({
+  url: "http://otel-collector.ts-observability.svc.cluster.local:4318/v1/metrics",
+  headers: {
+    "x-goog-project-id": process.env.GOOGLE_CLOUD_PROJECT || "",
+  },
+});
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
@@ -11,6 +20,10 @@ const sdk = new NodeSDK({
     headers: {
       "x-goog-project-id": process.env.GOOGLE_CLOUD_PROJECT || "",
     },
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+    exportIntervalMillis: 10000,
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
